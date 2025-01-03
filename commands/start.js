305279683553fa54595faa0da2b4e594f648112c
Nate <eklipz08@gmail.com>
2025-01-03 01:40:38 -0500

    refactor: Simplify vehicle selection logic and improve interaction handling in start command

--- commands/start.js
+++ commands/start.js
@@ -11,9 +11,6 @@ module.exports = {
     async execute(interaction) {
         let logger = await getLogger();
         let profile = await Profile.findOne({ userId: interaction.user.id });
-        if (profile && profile.userId !== '102688836454203392') {
-            return interaction.reply({ content: 'You already have a profile.', ephemeral: true });
-        }
 
         const starterCars = await Vehicle.find({ isStarterCar: true }).sort({ id: 1 }).lean();
         if (starterCars.length === 0) {
@@ -25,57 +22,50 @@ module.exports = {
             let pageIndex = 0;
             let e = await getStartEmbed(interaction, starterCars, pageIndex);
 
-            const message = await interaction.reply({ content: 'Please select your starter vehicle:', embeds: [e.embed], components: [e.row], files: [e.vehicleImage], fetchReply: true });
+            const message = await interaction.reply({ content: 'Please select your starter vehicle:', embeds: [e.embed], components: [e.row], files: [e.vehicleImage] });
             
             const filter = i => ['previous', 'next', 'select'].includes(i.customId) && i.user.id === interaction.user.id;
             const collector = message.createMessageComponentCollector({ filter, time: 60000 });
 
             collector.on('collect', async i => {
-                await i.deferUpdate();
-                switch (i.customId) {
-                    case 'previous':
-                        pageIndex = Math.max(pageIndex - 1, 0);
-                        break;
-                    case 'next':
-                        pageIndex = Math.min(pageIndex + 1, starterCars.length - 1);
-                        break;
-                    case 'select':
-                        const vehicle = starterVehicles[pageIndex];
-                        try {
-                            if (!vehicle) {
-                                await i.editReply({ content: "Selected vehicle not found.", components: [] });
-                                return;
-                            }
-                            profile = await Profile.create({
-                                userId: interaction.user.id,
-                                guildId: interaction.guildId,
-                                username: interaction.user.username,
-                                vehicles: [{ vehicleId: vehicle._id, year: vehicle.year, make: vehicle.make, model: vehicle.model, isActive: true, stats: vehicle.stats, image: vehicle.image }]
-                            });          
+                //await i.deferUpdate();
+                if (i.customId === 'previous') {
+                    pageIndex = Math.max(pageIndex - 1, 0);
+                } else if (i.customId === 'next') {
+                    pageIndex = Math.min(pageIndex + 1, starterCars.length - 1);
+                } else if (i.customId === 'select') {
+                    const vehicle = starterVehicles[pageIndex];
+                        if (!vehicle) {
+                            await i.editReply({ content: "Selected vehicle not found.", components: [] });
+                            return;
+                        }
+                        profile = await Profile.create({
+                            userId: interaction.user.id,
+                            guildId: interaction.guildId,
+                            username: interaction.user.username,
+                            vehicles: [{ vehicleId: vehicle._id, year: vehicle.year, make: vehicle.make, model: vehicle.model, isActive: true, stats: vehicle.stats, image: vehicle.image }]
+                        });          
             
-                            embed = new EmbedBuilder()
-                                .setColor('#00ff00')
-                                .setTitle(`Welcome to Octane ${profile.username} Racing!`)
-                                .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
-                                .setDescription('Your profile has been successfully set up!')
-                                .addFields(
-                                    { name: 'Free Vehicle', value: `You have received a free ${vehicle.make} ${vehicle.model}!` },
-                                    { name: 'Get Started', value: 'Use `/race` to start racing immediately' },
-                                    { name: 'Check Profile', value: 'Use `/profile` to view your profile and stats' },
-                                    { name: 'Buy Vehicles', value: 'Use `/cars` to view all vehicles' },
-                                    { name: 'Check Cooldowns', value: 'Use `/cooldowns` to view your cooldowns' }
-                                )
-                                .setFooter({ text: 'Type /help for a command list and more information' });
-                            await i.editReply({ content: '', embeds: [embed], components: [], files: [] });
-                            collector.stop();
-                        } catch (error) {
-                            logger.error(interaction.user.tag+' | start: '+error);
-                            return i.editReply({ content: 'Failed to create profile.', components: [] });
-                        }  
+                        embed = new EmbedBuilder()
+                            .setColor('#00ff00')
+                            .setTitle(`Welcome to Octane ${profile.username} Racing!`)
+                            .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+                            .setDescription('Your profile has been successfully set up!')
+                            .addFields(
+                                { name: 'Free Vehicle', value: `You have received a free ${vehicle.make} ${vehicle.model}!` },
+                                { name: 'Get Started', value: 'Use `/race` to start racing immediately' },
+                                { name: 'Check Profile', value: 'Use `/profile` to view your profile and stats' },
+                                { name: 'Buy Vehicles', value: 'Use `/cars` to view all vehicles' },
+                                { name: 'Check Cooldowns', value: 'Use `/cooldowns` to view your cooldowns' }
+                            )
+                            .setFooter({ text: 'Type /help for a command list and more information' });
+                        await i.editReply({ content: '', embeds: [embed], components: [], files: [] });
+                        collector.stop();
+                        return;
                 }
 
                 let e = await getStartEmbed(interaction, starterCars, pageIndex);
-                await i.editReply({ embeds: [e.embed], components: [e.row], files: [e.vehicleImage], fetchReply: true });
+                await i.update({ embeds: [e.embed], components: [e.row], files: [e.vehicleImage] });
             });
 
             collector.on('end', () => {
